92558cd86768e6c05746d28c9fc7765006ca0814
Roger Acosta <rogeracosta359@gmail.com>
2023-08-18 18:30:12 -0500

    php_crud_mysql

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Crud Mysql</title>
+  <!-- BOOSTRAP 5.3.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  <!-- Font AWESOME -->
+  <script src="https://kit.fontawesome.com/58adac63e1.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+  <nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+      <a href="index.php" class="navbar-brand">PHP MYSQL CRUD</a>
+    </div>
+  </nav>
---
+++ save_task.php
@@ -0,0 +1,29 @@
+<?php
+
+include('db.php');
+
+if (isset($_POST['save_task'])) {
+  // echo 'saving';
+
+  $title = $_POST['title'];
+  $description = $_POST['description'];
+
+  // echo $title;
+  // echo $description;
+
+  $query = "INSERT INTO task(title, description) VALUES ('$title', '$description')";
+  $result = mysqli_query($conn, $query);
+  if (!$result) {
+    die('Query Failed');
+  }
+  ;
+
+  $_SESSION['message'] = 'Task Saved succesfully';
+  $_SESSION['message_type'] = 'success';
+
+ header('Location: index.php');
+
+}
+;
+
+?>
